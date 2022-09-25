92c252f4f4d26f873a32216ca610e7bcb3dcebe6
Elena <MissKatharsis@yandex.ru>
2022-09-25 18:24:45 +0300

    add algorithm for bubbleSort

---
+++ src/bubbleSort.js
@@ -0,0 +1,24 @@
+const bubbleSort = (arr) => {
+    let stepsCount = arr.length - 1;
+    let swapped;
+
+    do {
+        swapped = false;
+
+        for (let i = 0; i < stepsCount; i += 1) {
+            if (arr[i] > arr[i + 1]) {
+                const temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+
+                swapped = true;
+            }
+        }
+
+        stepsCount -= 1;
+    } while (swapped);
+
+    return arr;
+};
+
+console.log(bubbleSort([14, 20, -10, -63, 0, 100, 33, -33]));
