a14f6b944b681c26a05c7a9363bb5f5eb1285c53
Elena <MissKatharsis@yandex.ru>
2022-10-22 12:50:22 +0300

    create hashTable algorithm and description for it

---
+++ src/hashTable.js
@@ -0,0 +1,30 @@
+import { make, set, get } from './hashTable.js';
+
+import crc32 from 'crc-32';
+
+
+const getIndex = (key) => Math.abs(crc32.str(key)) % 1000;
+
+const make = () => [];
+
+const set = (map, key, value) => {
+    const hash = getIndex(key);
+
+    if (!map[hash] || (map[hash] && map[hash][0] === key)) {
+        map[hash] = [key, value];
+
+        return true;
+    }
+
+    return false;
+};
+
+const get = (map, key, defaultValue = null) => {
+    const hash = getIndex(key);
+
+    return map[hash] && map[index][0] === key
+      ? map[index][1]
+      : defaultValue;
+};
+
+export { make, set, get };
